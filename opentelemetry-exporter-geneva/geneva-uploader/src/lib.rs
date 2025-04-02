f2c3c55dd069aa7c43306c0b2f4b93d23becf691
Lalit Kumar Bhasin <labhas@microsoft.com>
2025-04-02 22:55:41 +0000

    changes..

--- opentelemetry-exporter-geneva/geneva-uploader/src/lib.rs
+++ opentelemetry-exporter-geneva/geneva-uploader/src/lib.rs
@@ -1,7 +1,5 @@
 mod config_service;
 mod uploader;
 mod ingestion_service;
-/*pub use config_service::{
-    AuthMethod, GenevaConfigClient, GenevaConfigClientConfig, GenevaConfigClientError, IngestionGatewayInfo,
-};*/
+pub(crate) use config_service::client::{IngestionGatewayInfo, GenevaConfigClient};
 pub use uploader::{create_uploader, GenevaUploader};
